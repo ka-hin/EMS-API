c3e5225f772b6bd22545fc2d4a0a2b0da1e54273
ka-hin <khkahin139@gmail.com>
2020-04-01 11:15:01 +0800

    [Login] added md5 hash to read hashed password

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,11 +1,12 @@
 var mongoose = require('mongoose');
 var Employee = mongoose.model('employee');
+var md5 = require('md5');
 
 exports.getLoginDetails = async function(req,res){
     const LoginID = req.params.id;
     const pwd = req.params.pass;
 
-    await Employee.findOne({domain_id : LoginID, password : pwd, activated: true }, 'role -_id' , function(err, employee){
+    await Employee.findOne({domain_id : LoginID, password : md5(pwd), activated: true }, 'role -_id' , function(err, employee){
         if(err)
             res.send(err);
         res.json(employee);
