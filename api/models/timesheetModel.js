b3aedb36fcee6a62e90f1d721918f6b18d75f9fd
ka-hin <khkahin139@gmail.com>
2020-04-14 10:13:06 +0800

    [Clock In] Minor changes to timesheet schema

--- api/models/timesheetModel.js
+++ api/models/timesheetModel.js
@@ -4,24 +4,48 @@ var Schema = mongoose.Schema;
 
 
 var TimesheetSchema = new Schema({
-  date_in: {
-    type: String
+  domain_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'employee',
+      required: true
   },
-  date_out: {
-      type: String
+  date_in: {
+      type: String,
+      required: true
   },
   time_in: {
-      type: String
+      type: String,
+      required: false
   },
   time_out: {
-      type: String
+      type: String,
+      required: false
   },
-  period_id: {
-      type: String
+  date_out: {
+    type: String,
+    required: false
   },
-  domain_id: {
-      type: String
-  }
+  period_number: {
+      type: String,
+      required: true
+  },
+  year: {
+    type: String,
+    required: true
+  },
+  ot: {
+    type: Number,
+    required: true
+  },
+  ut: {
+    type: Number,
+    required: true
+  },
+  late: {
+    type: Number,
+    required: true
+ }
+  
 });
 
 module.exports = mongoose.model('timesheet', TimesheetSchema, 'timesheets');
