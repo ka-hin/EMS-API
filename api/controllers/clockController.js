a2c5a6474bbb51d273ef4eb855d6cf6ce9450588
Odale Roxas <roxasodale91@gmail.com>
2020-04-15 22:00:55 +0800

    [Clock In] Fix Timesheet Plotting and Clock In update

--- api/controllers/clockController.js
+++ api/controllers/clockController.js
@@ -4,7 +4,7 @@ var Timesheet = mongoose.model('timesheet');
 
 async function createPeriods(year,res){
 
-    await Period.findOne({"year":year},function(err, period){
+    await Period.findOne({"year":year}, function(err, period){
         if(err){
             res.status(500);
             res.send('There is a problem with the record');
@@ -29,75 +29,64 @@ async function createPeriods(year,res){
                         res.status(500);
                         res.send('There is a problem with the record');
                     }
-                    console.log(period);
                 });
             }
         }
     });
 };
 
-async function createTimesheet(domainID, periodNumber, year,res){
+async function createTimesheet(domainID, periodNumber, year,res) {
 
-    await Timesheet.findOne({"domain_id" : domainID, "period_number": periodNumber, "year": year}, function(err, timesheet){
-        if(err){
-            res.status(500);
-            res.send('There is a problem with the record');
-        }
+    // Check if there is a plotted timesheet for specified user and period.
+    const timesheet = await Timesheet.findOne({"domain_id" : domainID, "period_number": periodNumber, "year": year});
 
-        if(!timesheet){
-            Period.findOne({"period_number": periodNumber, "year": year},function(err,period){
-                if(err){
-                    res.status(500);
-                    res.send('There is a problem with the record!');
+    // If no timesheet, generate a timesheet in DB.
+    if (!timesheet) {
+
+        const period = await Period.findOne({"period_number": periodNumber, "year": year});
+
+        if(period) {
+
+            const startDate = Number(period.date_start.substr(0, 2));
+            const endDate = Number(period.date_end.substr(0, 2));
+
+            const new_timesheet = [];
+
+            for(var i = startDate; i <= endDate ; i++){
+                var dateIn = ("0"+ i).slice(-2)+"-"+("0"+(Number(periodNumber)+1)).slice(-2);
+                var d = new Date(Number(year), Number(periodNumber), i,+8);
+                var day = null;
+
+                if(d.getDay() === 0 || d.getDay() === 6){
+                    day = "Weekend";
                 }
 
-                if(period){
-                    var start = Number(period.date_start.substr(0,2));
-                    var end = Number(period.date_end.substr(0,2));
+                const timesheetObj = {
+                    "domain_id":domainID,
+                    "date_in":dateIn,
+                    "time_in":"0000",
+                    "time_out":"0000",
+                    "date_out":null,
+                    "period_number": periodNumber,
+                    "year": year,
+                    "ot":0,
+                    "ut":0,
+                    "late":0,
+                    "remarks":day
+                };
 
-                    for(var i = start; i<= end ; i++){
-                        var dateIn = ("0"+ i).slice(-2)+"-"+("0"+(Number(periodNumber)+1)).slice(-2);
-                        var d = new Date(Number(year), Number(periodNumber), i,+8);
-                        var day = null;
-                        if(d.getDay() === 0 || d.getDay() === 6){
-                            day = "Weekend";
-                        }
-                        var timesheetObj = {
-                            "domain_id":domainID,
-                            "date_in":dateIn,
-                            "time_in":"0000",
-                            "time_out":"0000",
-                            "date_out":null,
-                            "period_number": periodNumber,
-                            "year": year,
-                            "ot":0,
-                            "ut":0,
-                            "late":0,
-                            "remarks":day
-                        }
-                        var new_timesheet = new Timesheet(timesheetObj);
-                        new_timesheet.save(function(err, timesheet){
-                            if(err){
-                                res.status(500);
-                                res.send('There is a problem with the record');
-                            }
-                            console.log(timesheet);
-                        });
-                    }
-                }
-            });   
+                new_timesheet.push(new Timesheet(timesheetObj));
+            }
+
+            return new_timesheet;
         }
-    });
+    }
+
+    return [];
 };
 
 async function clockIn(domainID, dateIn, timeIn, year, res){
-    Timesheet.findOneAndUpdate({"domain_id": domainID, "date_in": dateIn, "year":year},{"time_in":timeIn},{new:true})
-        .then(function(changes){
-            res.send(changes);
-        }).catch(function(err){
-            res.status(500);
-            res.send(err);
-        });
+    return await Timesheet.findOneAndUpdate({"domain_id": domainID, "date_in": dateIn, "year":year},{"time_in":timeIn},{new:true});
 }
 
 exports.clockIn = async function(req,res){
@@ -108,6 +97,23 @@ exports.clockIn = async function(req,res){
     var period = (Number(dateIn.substr(3,2))-1).toString();
 
     const resolvePeriod = await createPeriods(year,res);
-    const resolveTimeSheet = await createTimesheet(domainID,period,year,res);
-    const resolveClockIn = await clockIn(domainID,dateIn, timeIn, year, res);
+    await createTimesheet(domainID,period,year,res).then( async (doc) => {
+
+        if(doc.length) {
+            doc.map( doc => {
+                if(doc.date_in === dateIn) {
+                    console.log(doc);
+                    doc.time_in = timeIn;
+                }
+    
+                return doc;
+            });
+
+            await Timesheet.insertMany(doc);
+        } else {
+            await clockIn(domainID,dateIn, timeIn, year, res);
+        }
+
+        res.send('Clocked In');
+    });  
 };
