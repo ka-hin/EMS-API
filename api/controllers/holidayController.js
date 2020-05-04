f442c1a0d544457e74b3e187cb21da37089dbe8c
ka-hin <khkahin139@gmail.com>
2020-05-04 14:14:00 +0800

    [Holiday] API to Update existing Timesheet's remark when new holiday is created

--- api/controllers/holidayController.js
+++ api/controllers/holidayController.js
@@ -23,6 +23,18 @@ exports.saveHoliday = async function(req, res){
         }
         
     }
-    
+    await updateTimesheetHoliday(saved);
     res.send(saved);
 };
+
+async function updateTimesheetHoliday(updates){
+    for(let i = 0 ; i < updates.length; i++){
+        if(updates[i] != null){
+            const timesheet = await Timesheet.find({date_in: updates[i].date, year: updates[i].year});
+
+            for(let j = 0; j < timesheet.length; j++){
+                await Timesheet.findOneAndUpdate({domain_id: timesheet[j].domain_id, date_in:timesheet[j].date_in, year: timesheet[j].year},{remarks: /*updates[i].holiday_name*/null},{new:true});
+            } 
+        }
+    }
+}
